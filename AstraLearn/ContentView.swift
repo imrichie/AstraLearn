6355af1f8196d3901dcba42f9bea096d809a1747
Richie Flores <rfloresc@icloud.com>
2024-03-27 21:18:09 -0700

    Add background with a space gradient theme

--- AstraLearn/ContentView.swift
+++ AstraLearn/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    // define the gradient colors
+    let deepSpaceBlue   = Color(red: 0.1, green: 0.1, blue: 0.3)
+    let galacticPurple  = Color(red: 0.4, green: 0.2, blue: 0.5)
+    let midnightBlue    = Color(red: 0.2, green: 0.2, blue: 0.4)
+    let nebulaPurple    = Color(red: 0.3, green: 0.3, blue: 0.5)
+    
+    var gradient: Gradient {
+        Gradient(colors: [deepSpaceBlue, galacticPurple, midnightBlue, nebulaPurple, deepSpaceBlue])
+    }
+    
+    var spaceGradient: LinearGradient {
+        LinearGradient(gradient: gradient, startPoint: .topLeading, endPoint: .bottomTrailing)
+    }
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background
+            spaceGradient
+                .ignoresSafeArea()
+            
+            // Main Content Layer
+            VStack {
+                Text("Hello, World!")
+                    .font(.largeTitle.weight(.thin))
+                    .foregroundStyle(.white)
+            }
         }
-        .padding()
     }
 }
 
