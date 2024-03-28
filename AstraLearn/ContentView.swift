8dacdbbca3cb710d10a2f8fd4fbd8ea0d74ee502
Richie Flores <rfloresc@icloud.com>
2024-03-27 22:49:15 -0700

    Add buttons for the respetive quizzes

--- AstraLearn/ContentView.swift
+++ AstraLearn/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    // State Variables
+    @State private var selectedCategory: String?
+    @State private var showQuiz: Bool = false
+    @State private var showAlert1: Bool = false
+    @State private var showAlert2: Bool = false
+    
     // define the gradient colors
     let deepSpaceBlue   = Color(red: 0.1, green: 0.1, blue: 0.3)
     let galacticPurple  = Color(red: 0.4, green: 0.2, blue: 0.5)
@@ -30,9 +36,50 @@ struct ContentView: View {
             
             // Main Content Layer
             VStack {
-                Text("Hello, World!")
-                    .font(.largeTitle.weight(.thin))
+                Text("Welcome to Astra Learn")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
                     .foregroundStyle(.white)
+                
+                Button(action: {
+                    showAlert1 = true
+                }, label: {
+                    Text("Planets Quiz")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(Color.blue)
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
+                        .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.white, lineWidth: 2))
+                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                })
+                .padding(.bottom)
+                .alert("Planet Quiz", isPresented: $showAlert1) {
+                    Button("Dismiss", role: .none) {}
+                } message: {
+                    Text("Quiz for the Planets")
+                }
+                
+                Button(action: {
+                    showAlert2 = true
+                }, label: {
+                    Text("Space Mission Quiz")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(Color.purple)
+                        .clipShape(RoundedRectangle(cornerRadius: 25))
+                        .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.white, lineWidth: 2))
+                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                })
+                .padding()
+                .alert("Space Mission Quiz", isPresented: $showAlert2) {
+                    Button("Dismiss", role: .none) {}
+                } message: {
+                    Text("Quiz for Space Missions")
+                }
             }
         }
     }
