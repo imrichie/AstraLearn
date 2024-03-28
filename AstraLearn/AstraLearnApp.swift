f16e97fda9352b6c18ab4dff8a980b1cf45cf0eb
Richie Flores <rfloresc@icloud.com>
2024-03-27 17:34:31 -0700

    Initial Commit

---
+++ AstraLearn/AstraLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstraLearnApp.swift
+//  AstraLearn
+//
+//  Created by Richie Flores on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AstraLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
